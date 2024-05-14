9c8b5ed40fa032ef8fcbc1feed55750efceddcdb
Ephraim Hsu <ehsu18@my.bcit.ca>
2024-05-14 14:43:37 -0700

    DEBUG favicon not showing up.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const path = require('path');
+const favicon = require('serve-favicon');
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -36,7 +38,7 @@ var mongoStore = MongoStore.create({
     }
 })
 
-app.use(favicon(path.join(__dirname, 'public', 'test.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.use(express.urlencoded({ extended: false }));
 
