fd4c93071196f18509ec871b88cd3ae4ab90febc
Michael <mcbride1987@gmail.com>
2024-05-15 11:37:06 -0700

    update weather to get icon, temperature, and city name. Also created a EJS page to display functionality along with a dropdown to show it change depending on reading a user selected city field.

--- weather.js
+++ weather.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
 
 const weatherKey = process.env.OPEN_WEATHER_API_KEY;
 
@@ -11,13 +13,16 @@ app.listen(3000, () => {
     console.log('Server running on port 3000');
     });
 
-app.get("/getWeatherOfACityByName", (req, res) => {
-    fetch (`https://api.openweathermap.org/data/2.5/weather?q=Vancouver, BC, CAS&appid=${weatherKey}`)
+app.get("/", (req, res) => {
+    fetch (`https://api.openweathermap.org/data/2.5/weather?q=${req.query.selectCity}, BC, CAS&appid=${weatherKey}&units=metric`)
     .then((current) => current.json())
     .then((current) => {
         if (current.message === 'city not found')
             return res.send('City not found')
+        const cityName = current.name
         const weatherToday = current.weather[0].main
-        res.send("The weather today is " + weatherToday)
+        const weatherTemp = current.main.temp
+        const weatherIcon = current.weather[0].icon
+        res.render('fitness_weather', {cityName, weatherToday, weatherTemp, weatherIcon})
     });
 });
