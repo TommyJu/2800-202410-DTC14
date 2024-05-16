7dae84ffdacd496f15755470cfb238bc5164d188
Michael McBride <mcbride1987@gmail.com>
2024-05-16 08:32:05 -0700

    Fix issue with initial page load not having a query paramenter

--- weather.js
+++ weather.js
@@ -11,18 +11,23 @@ app.use(cors());
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
-    });
+});
 
 app.get("/", (req, res) => {
-    fetch (`https://api.openweathermap.org/data/2.5/weather?q=${req.query.selectCity}, BC, CAS&appid=${weatherKey}&units=metric`)
-    .then((current) => current.json())
-    .then((current) => {
-        if (current.message === 'city not found')
-            return res.send('City not found')
-        const cityName = current.name
-        const weatherToday = current.weather[0].main
-        const weatherTemp = current.main.temp
-        const weatherIcon = current.weather[0].icon
-        res.render('fitness_weather', {cityName, weatherToday, weatherTemp, weatherIcon})
-    });
+    if (!req.query.selectCity) {
+        url = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver,BC,CA&appid=${weatherKey}&units=metric`
+    } else {
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${req.query.selectCity},CA&appid=${weatherKey}&units=metric`
+    }
+    fetch(url)
+        .then((current) => current.json())
+        .then((current) => {
+            if (current.message)
+                return res.send('Error')
+            const cityName = current.name
+            const weatherToday = current.weather[0].main
+            const weatherTemp = current.main.temp
+            const weatherIcon = current.weather[0].icon
+            res.render('fitness_weather', { cityName, weatherToday, weatherTemp, weatherIcon })
+        });
 });
