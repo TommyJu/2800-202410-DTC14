5446f5ab31f6d848f491f4143c67b13a3683f881
Ephraim Hsu <ehsu18@my.bcit.ca>
2024-05-16 22:45:46 -0700

    REMOVE unused code and comments.

--- riotLeagueAPI.js
+++ riotLeagueAPI.js
@@ -92,27 +92,3 @@ module.exports = {
   getMatchHistory,
   calculateWinLoss,
 };
-
-
-// Importing the API logic functions to link back end data to front end display.
-// const lolAPI = require('./riotLeagueAPI.js');
-
-
-// app.get("/tempGame", async (req, res) => {
-//   const PUUID = await lolAPI.getRiotPUUID(user_name, user_tag);
-//   const summonerDetails = await lolAPI.getSummonerLevelAndID(PUUID);
-//   const summonerLevel = summonerDetails[1];
-//   const encryptedSummonerId = summonerDetails[0];
-//   const summonerRank = await lolAPI.getSummonerRank(encryptedSummonerId);
-//   if (summonerRank === null) {
-//     var rank = "Unranked";
-//   } else {
-//     var rank = summonerRank[0] + " " + summonerRank[1];
-//   }
-//   const match_ids = await lolAPI.getMatchHistory(PUUID);
-//   const winrateAndKD = await lolAPI.calculateWinLoss(match_ids, PUUID);
-//   console.log(winrateAndKD);
-//   const winrate = winrateAndKD[0];
-//   const kd = winrateAndKD[1];
-//   res.render("tempGame.ejs", { level: summonerLevel, rank: rank, winrate: winrate, kd: kd});
-// })
